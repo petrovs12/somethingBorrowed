273393a9d414066b28802b1b759bc6e5381ae8c9
petrovs12 <stefanvpetrov@gmail.com>
2016-02-18 14:43:53 +0200

    added a bunch of isntructions and comments

--- modelling1.R
+++ modelling1.R
@@ -4,6 +4,7 @@ pacman::p_load("readxl","stringr","lubridate","data.table","zoo","xts","dplyr","
 timezone <- 
   dayFormat <- "%Y%m%d"
 
+# read holidays---
 dummies <- read_excel("Dummy_variables_v2.xlsx")
 dummies <- as.data.table(dummies)
 xts(dummies[,-"time_id",with=F],order.by = dummies[,strptime(time_id,dayFormat)])
@@ -12,13 +13,15 @@ dummiesSeries <- xts(
   ,order.by = dummies[,date])
 
 martaEncoding <- "[A-X].*_*[A-X].*"
+
+# all that are holidays- cluster holidays later to improve accuracy
+#summarise all holidays- kind of crude, better to split by religious/national and by apriori "impact
+# e.g. easter, christmass, new year, 3rd march etc national ones are stronger than rest but meh
 goodCols <- setdiff(grep(martaEncoding,colnames(dummies),value = T),c("Speedy_Working_Day","Working_Saturday"))
 goodCols
 
 # grep("[A-X].*_*[A-X].*","Asdkf_Balsdfj")
-
-#summarise all holidays- kind of crude, better to split by religious/national and by apriori "impact
-# e.g. easter, christmass, new year, 3rd march etc national ones are stronger than rest but meh
+# create the "is the day any sort of holiday" variable---------------
 dummies[,isAnyHoliday:=apply(.SD,1,max),.SDcols= goodCols ]# since 0 and 1 max works here
 
 # dummies[,dtime:=strptime(time_id,dayFormat,timezone)]
@@ -39,29 +42,31 @@ extractHub <- function(st) {
   if(length(ind)==0)0
   as.integer(paste0(str_sub(st,1,1),str_sub(st,3,ind[2])))
 }
-#SLOW! HOW TO MAKE FASTER??------------
+#create ids from the UTF- formatted names------------
+#SLOW! HOW TO MAKE FASTER??
 origins <- sapply(speedy$origin, extractHub)
 destinations <- sapply(speedy$destination, extractHub)
 speedy[,org:=origins]
 speedy[,dst:=destinations]
 speedy[,total:=express+economy]
 
-speedy$org %>% class
+speedy$org %>% class=="integer"
 
 # speedy[,extractHub(origins)] %>% table()
 # sanity check if all sectors appear for all dates ------------------------
 table(speedy[,.N,by=time_id]$N)
 
 speedyWithHolidays <- merge(speedy,dummies,by="time_id",all.x = TRUE,all.y = FALSE)
-speedyWithHolidays[Speedy_Working_Day==0,summary(economy)]
 
 # ups!!!- sanity check fails!!! (not by much though)!!!!!!!!!!!!- most still 0s, am i wrong?
+speedyWithHolidays[Speedy_Working_Day==0,summary(economy)]
 speedyWithHolidays[Speedy_Working_Day==0,table(economy)]
 speedyWithHolidays[Speedy_Working_Day==0,table(express)]
 
-# sometime it happened they worked- maybe just 2 days?
+
+# exploratory sometime it happened they worked- maybe just 2 days?----
 holidaysWrong <- speedyWithHolidays[Speedy_Working_Day==0&(economy>0|express>0),.(time_id,origin,destination,express,economy)]
-holidaysWrong <- speedyWithHolidays[Speedy_Working_Day==0&(economy>0|express>0),.(time_id,origin,destination,express,economy)]
+holidaysWrong
 
 
 
@@ -79,8 +84,17 @@ speedyWithHolidays[,economy:=ifelse(Speedy_Working_Day>0,economy,0)]
 # speedyCast <- dcast.data.table(
 #   speedyWithHolidays,time_id~origin+
     # destination,value.var = c("economy","express"),fun.aggregate = list(sum,sum))[order(time_id)]
-speedyCast <- dcast.data.table(speedyWithHolidays,time_id~org + dst,value.var = c("economy","express","total"),fun.aggregate = list(sum,sum,sum))[order(time_id)]
 
+
+
+
+# convert from long to wide format ----------------------------------------
+speedyCast <- dcast.data.table(
+  speedyWithHolidays,time_id~org + dst,value.var = c("economy","express","total"),
+  fun.aggregate = list(sum,sum,sum))[order(time_id)]
+
+
+# create the aggregated data ----------------------------------------------
 speedyAgg <- speedy[,.(time_id,totalEcon=sum(economy),totalExpress=sum(express),total=sum(total)),by=time_id]
 
 speedyAggByOrigin <- speedy[,.(time_id,
@@ -94,13 +108,9 @@ speedyAggByDestination <- speedy[,.(time_id,
                                     totalEcon=sum(economy),
                                     totalExpress=sum(express)),by=.(time_id,dst)]
 
-speedyAgg[,tot:=totalEcon+totalExpress]
-speedyAggByOrigin[,tot:=totalEcon+totalExpress]
-speedyAggByDestination[,tot:=totalEcon+totalExpress]
-
-
 setkey(speedyAgg,"time_id")
 
+#exploratory
 speedyCast[1:30,1:3,with=F]
 colnames(speedyCast)
 nrow(speedyCast)
@@ -108,10 +118,12 @@ nrow(speedyCast)
 
 # check -------------------------------------------------------------------
 length(speedyCast$time_id)==length(unique(speedyCast$time_id))
+class(speedyCast$time_id)==class(dummies$time_id)
 
 setkey(speedyCast,"time_id")
 setkey(dummies,"time_id")
-class(speedyCast$time_id)==class(dummies$time_id)
+
+
 
 
 # merge with the holidays data, and then add the total daily volumes as well -------------------------------------------------------------------
@@ -130,13 +142,15 @@ all.equal(sort(unique(speedyCast$time_id)),sort(unique(speedy$time_id)))
 speedyCast[100:140,1:3,with=F]
 speedy[order(time_id),][160:210,economy]
 options(encoding="UTF-8")
-speedyCast[,summary(`economy.1_2-1 ПЛОВДИВ_2-4 ВАРНА`),with=F]
-# speedyCast$"economy.1_2-1 П2-1 ПЛОВДИ_2-10 РУСЕ"
+# speedyCast[,summary("economy.1_2-1 ПЛОВДИВ_2-4 ВАРН),with=F]
+# speedyCast$
+ # speedyCast$economy.1_2-1 П2-1 ПЛОВДИ_2-10 РУСЕ"
 # speedyCast$"economy.1_2-1 ПЛОВДИВ_2-4 ВАРНА"
 # speedyCast$"economy_2-1 ПЛОВДИВ_2-4 ВАРНА"
 
-targetOrigin <- 22#Plovdiv
-targetDest <- 24#Varna
+# NB! select sector here------------------------
+targetOrigin <- 22#Sofia
+targetDest <- 26#Varna
 
 
 
@@ -147,14 +161,18 @@ speedyCast[6:10,1:5,with=F]
 # explore plovdiv-varna ---------------------------------------------------
 
 colnames(speedyCast)
+
+# select which series to predict-econ, express or total, here -------------
 expressOrEcon <- "express_sum"
 # expressOrEcon <- "economy_sum"
+# expressOrEcon <- "total_sum"
 targetVolumedStable <- 
   speedyCast[order(time_id)][,
                                   paste0(expressOrEcon,"_",as.character(targetOrigin),"_",as.character(targetDest))
                                   ,with=F
                                   ]
 
+#check
 testpv <- 
   speedyCast[order(time_id)][,
                                   paste0("express_sum","_",as.character(targetOrigin),"_",as.character(targetDest))
@@ -171,10 +189,13 @@ tsSeries <- xts(cbind(
   # speedyCast$"economy_2-2 СОФИЯ_2-1 ПЛОВДИВ",
   # speedyCast$"economy_sum_2-4 ВАРНА_2-1 ПЛОВДИВ",
   targetVolumedStable,
-  speedyCast[,.(isAnyHoliday,Speedy_Working_Day)]),strptime(speedyCast$time_id,dayFormat))
+  speedyCast[,.(isAnyHoliday,Speedy_Working_Day
+                # ,Working_Saturday #and whatever else
+                )]),strptime(speedyCast$time_id,dayFormat))
 tsSeries[,-1]
 
 
+# add opposite direction as predictor ----------
 oppositeVolumesData <- speedyCast[order(time_id)][,
                                   paste0(expressOrEcon,"_",as.character(targetDest),"_",as.character(targetOrigin))
                                   ,with=F
@@ -187,7 +208,7 @@ tsSeriesAggregated <-lag.xts(
     speedyCast[order(time_id)][,.(totalEcon,totalExpress)]
     ,totalOrigin=speedyAggByOrigin[order(time_id)][org==targetOrigin,.(totExpOrigin=totalExpress,totEconOrigin=totalEcon)],
             totalDest=speedyAggByDestination[order(time_id)][dst==targetDest,.(totEconOrigin=totalExpress,totEconDest=totalEcon)],
-    oppositeVolumesData
+    oppositeVolumesData# remove if desired
   )
       , strptime(speedyCast[order(time_id)]$time_id,dayFormat)) ,1) %>% na.fill(0)
 
@@ -226,6 +247,7 @@ y <- tsSeries[,1]
 
 # functions to construct the time-based lags of what we're predicting----------------
 getLastWeek <- function(x) {
+  # x <- 10
   currInd <- index(y[x]) 
   dayweekind <- .indexwday(y[x]) 
   indx <- which((.index(y)<currInd)&(.indexwday(y)==dayweekind))
@@ -290,7 +312,7 @@ lastYearSameDaySeries
 constructAdditionalData <- function(baseSeries=y){
   # can simply do this, cause we rely on having daily observations for each sector, and all weeks have 7 days (doh:P)
   weeklyLags <- do.call(merge.xts,
-                        lapply(3:7,function(i){
+                        lapply(1:7,function(i){
                           ijj <- 7*i
                           lag(baseSeries,ijj)
                         })
@@ -306,17 +328,26 @@ constructAdditionalData <- function(baseSeries=y){
 }
 
 additionalYBasedData <- constructAdditionalData(y)
+#check
 (index(additionalYBasedData)==index(y) )%>% all()
 
-
+# change periods here------------------
 ytrainingPeriod <- "2015-02-01/2015-09-17"
 ytestingPeriod <- "2015-09-18/2015-10-14"
+#NB!!
+#construct a TS class that is suitable for Arima to detect seasonality on, 
+# though it breaks the real time indices
 yhat <- ts(coredata(tsSeries[ytrainingPeriod,1]),start=2013,frequency = 7)
 # yhat <- tsSeries[ytrainingPeriod,1]
 yhattest <- tsSeries[ytestingPeriod,1]
 # mod <- auto.arima(y["2014-01-01/2015-10-01"],xreg = embed(tsSeries["2013-12-29/2015-10-01",2:3],4),seasonal = T)
 nrow(yhat)+nrow(yhattest)-nrow(tsSeries)
 
+
+
+
+# todo- make this simpler using datetime arithmetics, rather than doing it always by hand
+#periods mismatch due to the embedding
 
 holidayPeriod <- "2015-01-30/2015-09-19"
 holidayPeriodTest <- "2015-09-16/2015-10-16"
@@ -336,18 +367,24 @@ nrow(holidayRelatedTest)==nrow(yhattest)
 
 # add overall volume-past 2 days ------------------------------------------------------
 
-aggregateStatsPeriod<-"2015-02-01/2015-09-17"
-aggregateStatsTestPeriod<-"2015-09-18/2015-10-14"
+# aggregateStatsPeriod<-"2015-02-01/2015-09-17"
+# aggregateStatsTestPeriod<-"2015-09-18/2015-10-14"
 aggregateStats2lags <- (
   merge.xts(tsSeriesAggregated,
                                  merge.xts( 
                                   lag.xts(tsSeriesAggregated,1),
-                                  lag.xts(tsSeriesAggregated,2))
+                                  lag.xts(tsSeriesAggregated,2),
+                                  lag.xts(tsSeriesAggregated,3),
+                                  lag.xts(tsSeriesAggregated,4),
+                                  lag.xts(tsSeriesAggregated,5)
+                                  )
                                  
-) %>% na.fill(0))
-aggregateStatsTrain <- aggregateStats2lags[aggregateStatsPeriod]
-aggregateStatsTest <- aggregateStats2lags[aggregateStatsTestPeriod]
+) %>% na.fill(0))#todo explore other na.fill optionhs, such as na.locf
 
+aggregateStatsTrain <- aggregateStats2lags[ytrainingPeriod,]
+aggregateStatsTest <- aggregateStats2lags[ytestingPeriod,]
+
+# must be true
 nrow(aggregateStatsTest)==nrow(yhattest)
 nrow(holidayRelatedTest)==nrow(yhattest)
 
@@ -360,6 +397,7 @@ nrow(holidayRelated)==nrow(yhat)
 # regressorTable <- cbind( coredata(holidayRelated),coredata(aggregateStatsTrain))
 # regressorTabletest <- cbind(coredata(holidayRelatedTest),coredata(aggregateStatsTest))
 
+# create design matrices --------------------------------------------------
 regressorTable <- cbind( 
   coredata(additionalYBasedDataTrain)
   ,          coredata(holidayRelated)
@@ -368,19 +406,22 @@ regressorTable <- cbind(
 
 regressorTabletest <- cbind(
   coredata(additionalYBasedDataTest)# ,
-  , coredata(holidayRelatedTest)
+  ,coredata(holidayRelatedTest)
   ,coredata(aggregateStatsTest)
 )
 
+#checks
 ncol(regressorTable)==ncol(regressorTabletest)
-nrow(regressorTable)
-nrow(regressorTabletest)
-ncol(regressorTabletest)
-ncol(regressorTabletest)
+# nrow(regressorTable)
+# nrow(regressorTabletest)
+# ncol(regressorTabletest)
+# ncol(regressorTabletest)
 
+# main call to the model.-------------------
+# sometimes this breaks, but the commented call bellow doesn't . use the commented if so
 mod <- auto.arima(yhat,xreg = regressorTable,seasonal = T,ic="aic",test="adf",allowmean = TRUE,allowdrift = TRUE,
                   max.p=5,max.q = 5,max.d=5,max.D=6,max.P=5,max.Q = 8)
-# mod <- auto.arima(yhat,xreg = regressorTable,seasonal = T,allowmean = TRUE,allowdrift = TRUE)
+mod <- auto.arima(yhat,xreg = regressorTable,seasonal = T,allowmean = TRUE,allowdrift = TRUE)
 summary(mod)
 # fct <- forecast(mod,xreg=embed(tsSeries['2015-08-29/',2:3],4))
 
@@ -406,6 +447,7 @@ matplot(cbind(pred,coredata(yhattest)
 err <- sum(abs((pred)-coredata(yhattest)))/sum(yhattest)
 err
 
+# normality check ---
 tseries::jarque.bera.test(resid(fct))
 plot(density(resid(fct)))
 plot(resid(fct))
